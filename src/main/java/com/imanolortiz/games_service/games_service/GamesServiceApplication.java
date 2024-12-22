5e524484f676cc906992adb3d3013ffc98d6a2c4
imanolortizfernandez <imanolof29@gmail.com>
2024-12-22 20:57:28 +0100

    first commit

---
+++ src/main/java/com/imanolortiz/games_service/games_service/GamesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.imanolortiz.games_service.games_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imanolortiz/games_service/games_service/GamesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imanolortiz.games_service.games_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
